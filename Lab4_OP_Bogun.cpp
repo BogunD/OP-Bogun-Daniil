6b091dcafe16bc1dd52abb7e935536f648a8dee0
BogunD <90133397+BogunD@users.noreply.github.com>
2021-10-15 20:48:38 +0300

    Add files via upload

---
+++ Lab4_OP_Bogun.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+#include <windows.h>
+
+int main()
+{
+    using namespace std;
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int n, k = 1 ;
+    double sum = 0;
+    do {
+        cout << "Введіть натуральне число n \n n = ";  cin >> n;
+        if ( n < 0 || n > 9 )
+            cout << "Помилка введення.";
+    } while ( n < 0 || n > 9 );
+
+    for (k; k <= n; k++)
+    {
+        sum += 1. / (k * pow((2 * k + 1), 2));
+    }
+
+    cout << "Sum = " << sum << endl;
+    system("pause");
+    return 0;
+    
+}
