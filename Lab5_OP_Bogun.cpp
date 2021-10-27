739451a882a17096ea6f316be3349b2f25a07096
BogunD <90133397+BogunD@users.noreply.github.com>
2021-10-27 20:48:30 +0300

    Add files via upload

---
+++ Lab5_OP_Bogun.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <windows.h>
+
+int main()
+{
+    using namespace std;
+    int a = 22199;
+
+    for (a; a < 22300; a++) {
+        for (a; a % 15 == 0; a++) {
+            cout << a << endl;
+        }
+    }
+}
