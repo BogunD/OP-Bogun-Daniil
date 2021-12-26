5b83c26de5a4b36ae2153c345a3bb8002ed08769
BogunD <90133397+BogunD@users.noreply.github.com>
2021-12-26 10:18:19 +0200

    Add files via upload

---
+++ Lab9_OP_Bogun.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <string>
+
+int FindSum(char str[], std::string mas);
+int main() {
+	char s[255];
+	std::string arr;
+	std::cout << "Enter groups of numbers: "; gets_s(s);
+	int i = FindSum(s, arr);
+	std::cout << "Sum = "<< i;
+
+}
+int FindSum(char str[], std::string mas) {
+	int sum = 0;
+	int currentNum = 0;
+	const char* delim = " ";
+	char* curr_ptr;
+	char* next_ptr = NULL;
+	curr_ptr = strtok_s(str, delim, &next_ptr);
+	while (curr_ptr != NULL) {
+		currentNum = std::stoi(curr_ptr);
+		sum += currentNum;
+		curr_ptr = strtok_s(NULL, delim, &next_ptr);
+	}
+	return sum;
+}
